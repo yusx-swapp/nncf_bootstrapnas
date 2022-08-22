fbdf62b3a284b6e3b73a3e25eb4e3cbd3cb84c64
J. Pablo Munoz <pablo.munoz@intel.com>
2022-08-22 12:18:49 -0700

    Fix return values in search

--- nncf/experimental/torch/nas/bootstrapNAS/search/search.py
+++ nncf/experimental/torch/nas/bootstrapNAS/search/search.py
@@ -362,6 +362,8 @@ class SearchAlgorithm(BaseSearchAlgorithm):
                                                                             self._elasticity_ctrl)
         self._evaluator_handlers.append(self._efficiency_evaluator_handler)
         self._evaluator_handlers.append(self._accuracy_evaluator_handler)
+        self.maximal_vals = [evaluator_handler.current_value for evaluator_handler
+                                  in self._evaluator_handlers]
 
         self._problem = SearchProblem(self)
         self._result = minimize(self._problem, self._algorithm,
@@ -373,14 +375,16 @@ class SearchAlgorithm(BaseSearchAlgorithm):
         if self.best_config is not None:
             self._elasticity_ctrl.multi_elasticity_handler.activate_subnet_for_config(self.best_config)
             self.bn_adaptation.run(self._model)
+            ret_vals = self.best_vals
         else:
             nncf_logger.warning("Couldn't find a subnet that satisfies the requirements. Returning maximum subnet.")
             self._elasticity_ctrl.multi_elasticity_handler.activate_maximum_subnet()
             self.bn_adaptation.run(self._model)
             self.best_config = self._elasticity_ctrl.multi_elasticity_handler.get_active_config()
             self.best_vals = [None, None]
+            ret_vals = self.maximal_vals
 
-        return self._elasticity_ctrl, self.best_config, [abs(elem) for elem in self.best_vals if elem is not None]
+        return self._elasticity_ctrl, self.best_config, [abs(elem) for elem in ret_vals if elem is not None]
 
     def visualize_search_progression(self, filename='search_progression') -> NoReturn:
         """
